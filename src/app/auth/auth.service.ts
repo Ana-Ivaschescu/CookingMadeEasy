93d54ff70101ff574b9fae22803ee96db85fea30
Ana <anaiv9cs@yahoo.co.uk>
2021-01-07 14:03:07 +0200

    added firebaseAPIkey as environment variable

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -6,6 +6,7 @@ import { catchError, tap } from 'rxjs/operators';
 import { BehaviorSubject, throwError } from 'rxjs';
 import { User } from './user.model';
 import { Router } from '@angular/router';
+import { environment } from '../../environments/environment'
 
 export interface AuthResponseData{
     kind: string;
@@ -29,7 +30,7 @@ export class AuthService{
 
     signup(email: string, password: string){
        return this.http.post<AuthResponseData>(
-            'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBxlurC69snBkuTPx-mIRobgPaQ4Tkr_pE',
+            'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + environment.firebaseAPIKey,
         {
             email: email,
             password: password,
@@ -51,7 +52,7 @@ export class AuthService{
 
     login(email: string, password: string) {
         return this.http.post<AuthResponseData>(
-            'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBxlurC69snBkuTPx-mIRobgPaQ4Tkr_pE',
+            'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + environment.firebaseAPIKey,
         {
             email: email,
             password: password,
