fed3a37f90d5384c08ea38ae0756176f99bafcb6
Ana <anaiv9cs@yahoo.co.uk>
2021-01-07 14:24:47 +0200

    fixed a bug

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -96,4 +96,8 @@ export class RecipeEditComponent implements OnInit {
       'ingredients': recipeIngredients
     });
   }
+
+  get controls() { // a getter!
+    return (<FormArray>this.recipeForm.get('ingredients')).controls;
+  }
 }
